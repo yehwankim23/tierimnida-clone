68db45729df991a59665599c90bf1d9e4603549d
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2023-05-21 11:10:35 +0900

    :bug: Fix badge on startup

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,24 @@
-function setSortByLevel(sortByLevel) {
-  chrome.storage.local.set({ sortByLevel: sortByLevel });
-  chrome.action.setBadgeText({ text: sortByLevel ? "ON" : "OFF" });
-  chrome.action.setBadgeBackgroundColor({ color: sortByLevel ? "green" : "red" });
+async function setSortByLevel(sortByLevel) {
+  await chrome.storage.local.set({ sortByLevel: sortByLevel }).then(() => {
+    chrome.action.setBadgeText({ text: sortByLevel ? "ON" : "OFF" });
+    chrome.action.setBadgeBackgroundColor({ color: sortByLevel ? "green" : "red" });
+  });
 }
 
-chrome.runtime.onInstalled.addListener(() => {
-  setSortByLevel(false);
+async function getSortByLevel() {
+  return await chrome.storage.local.get(["sortByLevel"]).then((result) => {
+    return result.sortByLevel;
+  });
+}
+
+chrome.runtime.onInstalled.addListener(async () => {
+  await setSortByLevel(false);
 });
 
-chrome.action.onClicked.addListener((_) => {
-  chrome.storage.local.get(["sortByLevel"]).then((result) => {
-    setSortByLevel(!result.sortByLevel);
-  });
+chrome.runtime.onStartup.addListener(async () => {
+  await setSortByLevel(await getSortByLevel());
+});
+
+chrome.action.onClicked.addListener(async (_) => {
+  await setSortByLevel(!(await getSortByLevel()));
 });
